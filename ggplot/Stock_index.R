dfa8736733fb533426ed2ae9ef5f45db630c43ad
Gaurav <gaurav@gaurav-VirtualBox.(none)>
2013-06-17 01:40:46 -0400

    ggplot homework

--- ggplot/Stock_index.R
+++ ggplot/Stock_index.R
@@ -1,13 +1,8 @@
 library(gdata)
 ## Read the original Excel sheet as is  ##
-index_data <-read.xls('./data_science_homework/ggplot/data_akbilgic.xlsx',sheet=1)
+index_data <-read.xls('./data_akbilgic.xlsx',sheet=1)
 
-## Add Two new Columns  ##
-index_data["Date_readable"] <- NA
-index_data["weekday"] <- NA
-
-## Add Values to new columns ##
-  # The dates in the orinal excel sheet are in some numeric format ..I think epoc date #
+## Add two new columns : Readable dates and the day of the week
 
 index_data$Date_readable <- as.Date(index_data$date, origin="1899-12-30")
 index_data$weekday <- weekdays(index_data$Date_readable)
@@ -18,12 +13,12 @@ mon_fri_mkt <-subset(index_data , index_data$weekday=="Monday"|index_data$weekda
 ## plot the vlaues in a Bar-graph ##
 ggplot(data = mon_fri_mkt ) +
   aes(x= Date_readable , y =FTSE ) + 
-  xlab("Dates") + ylab("Index Value")+ ggtitle("Monday Effect OR Weekend Effect") +
-  geom_bar(colour="black",  
+  xlab("Dates") + ylab("Index Value:FTSE 100")+ ggtitle("Monday Effect OR Weekend Effect") +
+  geom_bar(  
            aes(fill=weekday),
            stat="identity",
-           position=position_dodge()
-           )
+           position=position_dodge() 
+           )+theme_bw()
 
 
 
